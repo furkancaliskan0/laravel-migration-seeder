6ddc1b5a008f9c23220e44408c5344d84b59fe43
furkanGit <furkancaliskan9801@gmail.com>
2023-02-01 18:11:32 +0100

    creation of migrations

---
+++ database/migrations/2023_02_01_142223_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->dateTime('Orario di partenza');
+            $table->dateTime('Orario di arrivo');
+            $table->string('Codice Treno');
+            $table->string('Numero Carrozze');
+            $table->string('In orario');
+            $table->string('Cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
