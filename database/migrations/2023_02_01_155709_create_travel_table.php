57481bc802b73466d67cc8d68004dcb6d39988f5
furkanGit <furkancaliskan9801@gmail.com>
2023-02-02 19:09:23 +0100

    Created Travel Migration

---
+++ database/migrations/2023_02_01_155709_create_travel_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency');
+            $table->string("departure_station");
+            $table->string("arrival_station");
+            $table->integer('duration')->unsigned();
+            $table->string("destination");
+            $table->integer('price')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+};
